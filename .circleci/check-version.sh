f7f3967d4726b9ffa2790c83d2346ee0bc727a74
Direside <shane.gearon@gmail.com>
2020-01-02 18:06:54 -0800

    Removed the git diff portion as there is no way to easily tell when multiple commits are pushed to master.

--- .circleci/check-version.sh
+++ .circleci/check-version.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # Checks if your orb has a new version number or if it's a duplicate of the latest.
-
 ORB_REF=$1
-ORB_DATA=$(circleci orb info "$ORB_REF" &>/dev/null || echo "Not found")
-LATEST=$( echo "$ORB_DATA" | grep -E 'Latest: .*@([0-9.]+)' | cut -d @ -f 2 )
 
-if [ "$(cat version.txt)" == "$LATEST" ]; then
-    echo "Error: Version is the same as published. Please update version.txt"
-    exit 1
+ORB_DATA=$(circleci orb info "$ORB_REF" 2>/dev/null || echo "Not found")
+LATEST=$( echo "$ORB_DATA" | grep -E 'Latest: .*@([0-9.]+)' | cut -d @ -f 2 )
+VERSION=$(cat "src/$ORB_REF/version.txt")
+
+if [ "$VERSION" == "$LATEST" ]; then
+    echo "Version is the same as published. Please update version.txt if you need to publish this orb."
+    exit 0
 fi
+
+echo "updated"
